50071b40019004dc7ebdbd851c1a9aa9a38ea809
Malindu Shamalka <mshamalka2@gmail.com>
2021-07-21 13:06:47 +0530

    API call initialized

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(()=>{
-    hello();
+    
+    const hello =async()=>{
+        const data = await fetch('https://www.hpb.health.gov.lk/api/get-current-statistical')
+        if(data){
+            alert('tada')
+        }else{
+            alert('kora')
+        }
+     }
+   
+     hello();
 })
 
-const hello =async()=>{
-   const data = await fetch('https://www.hpb.health.gov.lk/api/get-current-statistical')
-   if(data){
-       alert('tada')
-   }else{
-       alert('kora')
-   }
-}
